1e9ab47d4e0446e05596213e08e41baa0d363d9a
wujie <mjzhutianxiu@163.com>
2021-03-08 22:36:49 +0800

    Project has been created and configed.We also imported the element-UI model.

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">  //scoped 的有无代表了样式是全局性还是局部性，存在 scoped 即是局部性变量
+</style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello world! Please login here!
+  </div>
+</template>
+<script>
+export default {
+  name: "Login"
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
